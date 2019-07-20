1c5edae2ef02d02c9bf0530db6be32b5227e765a
Mahbub Munna <mahbubhassanmunna@gmail.com>
2019-07-20 23:32:23 +0600

    Lottie Animation added

--- app/src/main/java/me/moonss/hotspottoggler/ui/MainActivity.java
+++ app/src/main/java/me/moonss/hotspottoggler/ui/MainActivity.java
@@ -1,33 +1,43 @@
 package me.moonss.hotspottoggler.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import me.moonss.hotspottoggler.R;
 import me.moonss.hotspottoggler.utilities.TetheringUtils;
 import me.moonss.hotspottoggler.utilities.WifiApManager;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
     private AdView mAdView;
-    private ImageView tetheringImage;
+    private LottieAnimationView tetheringAnim;
     private TextView tetheringText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tetheringImage = findViewById(R.id.tetheringImg);
+
+        //Setting toolbar as action bar
+        Toolbar toolbar = findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        //Setting Image Views
+        tetheringAnim = findViewById(R.id.tetheringAnim);
         tetheringText = findViewById(R.id.messageTextView);
 
 
@@ -35,10 +45,19 @@ public class MainActivity extends AppCompatActivity {
         WifiApManager apManager = new WifiApManager(this);
         boolean isApEnabled = apManager.isWifiApEnabled();
         if (isApEnabled) {
-            tetheringImage.setImageResource(R.drawable.ic_wifi_tethering_black_24dp);
-            tetheringText.setText(getString(R.string.tethering_text));
-            tetheringText.setTypeface(null, Typeface.BOLD);
+            tetheringAnim.setAnimation("animation-hotspot-green.json");
+            tetheringAnim.playAnimation();
+            tetheringAnim.setRepeatCount(LottieDrawable.INFINITE);
+            tetheringText.setText(getString(R.string.tethering_text_on));
         }
+
+        //Click handle of Tethering Animation
+        tetheringAnim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTethering(v);
+            }
+        });
 
         initializeGoogleAdd();
     }
@@ -55,15 +74,20 @@ public class MainActivity extends AppCompatActivity {
         WifiApManager wifiApManager = new WifiApManager(this);
         if (!wifiApManager.isWifiApEnabled()) {
             TetheringUtils.startTetheringService(this);
-            tetheringImage.setImageResource(R.drawable.ic_wifi_tethering_black_24dp);
-            tetheringText.setText(getString(R.string.tethering_text));
-            tetheringText.setTypeface(null, Typeface.BOLD);
+           // tetheringAnim.setImageResource(R.drawable.ic_wifi_tethering_black_24dp);
+            tetheringAnim.setAnimation("animation-hotspot-green.json");
+            tetheringAnim.playAnimation();
+            tetheringAnim.setRepeatCount(LottieDrawable.INFINITE);
+            tetheringText.setText(getString(R.string.tethering_text_on));
 
         } else {
             TetheringUtils.stopTetheringService(this);
-            tetheringImage.setImageResource(R.drawable.ic_portable_wifi_off_black_24dp);
+            //tetheringAnim.setImageResource(R.drawable.ic_portable_wifi_off_black_24dp);
+            tetheringAnim.pauseAnimation();
+            tetheringAnim.setAnimation("animation-hotspot-red.json");
+            tetheringAnim.playAnimation();
+            tetheringAnim.pauseAnimation();
             tetheringText.setText(getString(R.string.not_tethering_text));
-            tetheringText.setTypeface(null, Typeface.NORMAL);
         }
 
     }
@@ -78,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+
         switch (id) {
             case R.id.action_settings:
                 Intent startSettingActivity = new Intent(this, SettingsActivity.class);
@@ -87,7 +112,22 @@ public class MainActivity extends AppCompatActivity {
                 Intent startAboutActivity = new Intent(this, AboutActivity.class);
                 startActivity(startAboutActivity);
                 return true;
+            case R.id.action_privacy_policy:
+                String url = "http://moonss.me/apps/wifi_hotspot/privacy_policy/";
+                Uri webPage = Uri.parse(url);
+                Intent startPrivacyPolicy =
+                        new Intent(Intent.ACTION_VIEW, webPage);
+                if (startPrivacyPolicy.
+                        resolveActivity(getPackageManager()) != null)
+                {
+                    startActivity(startPrivacyPolicy);
+                }
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
